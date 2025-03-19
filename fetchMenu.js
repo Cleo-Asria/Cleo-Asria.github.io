ae4be77f24c1a7cb1d8e843c91f2326b8a2d93fa
Judy <briggstaillefer10@gmail.com>
2025-03-18 17:54:22 -0700

    Update fetchMenu.js

--- fetchMenu.js
+++ fetchMenu.js
@@ -1,7 +1,12 @@
-fetchMenu('https://GeekySans.github.io/menu.txt');
+fetchMenu('https://GeekySans.github.io/menu.txt')
+.then(displayMenu)
 
 async function fetchMenu(file) {
   let myFile = await fetch(file);
+  return await myFile
+}
+
+function displayMenu {
   let myHTML = await myFile.text();
   document.getElementById("demo").innerHTML = myHTML;
 }
