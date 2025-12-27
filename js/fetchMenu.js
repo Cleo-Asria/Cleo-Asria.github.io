0e013e7a6c22d6029f12b42244d9189124bba630
Judy <briggstaillefer10@gmail.com>
2025-12-27 12:26:01 -0800

    Update fetchMenu.js

--- js/fetchMenu.js
+++ js/fetchMenu.js
@@ -1,14 +1,3 @@
-fetchHTMLContent('../../../js/menu.html')
-.then(displayHTMLContent);
-
-async function fetchHTMLContent(file) {
-  let myFile = await fetch(file);
-  return await myFile
-}
-
-async function displayHTMLContent(myFile) {
-  insertedHTML = await myFile.text();
-  currentNode = document.getElementsByClassName('menu')[0];
-  currentHTML = currentNode.innerHTML;
-  currentNode.innerHTML = currentHTML + insertedHTML;
-}
+let resultFile = fetchHTMLContent('../../../js/menu.html');
+let insertInto = document.getElementById('menu');
+displayHTMLContent(resultFile, insertInto);
