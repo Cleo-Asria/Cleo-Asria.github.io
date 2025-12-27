1a165dbae81441691dc1899e65fd6e5725a4a403
Judy <briggstaillefer10@gmail.com>
2025-12-27 12:24:49 -0800

    Update fetchFont.js

--- js/fetchFont.js
+++ js/fetchFont.js
@@ -1,14 +1,3 @@
-fetchHTMLContent('../../../js/getfont.html')
-.then(displayHTMLContent);
-
-async function fetchHTMLContent(file) {
-  let myFile = await fetch(file);
-  return await myFile
-}
-
-async function displayHTMLContent(myFile) {
-  insertedHTML = await myFile.text();
-  currentNode = document.head;
-  currentHTML = currentNode.innerHTML;
-  currentNode.innerHTML = currentHTML + insertedHTML;
-}
+let resultFile = fetchHTMLContent('../../../js/getfont.html');
+let insertInto = document.head;
+displayHTMLContent(resultFile, insertInto);
