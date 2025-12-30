9abedeaa79103d7c9d9f9969c61d0468f74322c0
Judy <briggstaillefer10@gmail.com>
2025-12-29 19:07:46 -0800

    Update fetchMenu.js

--- js/fetchMenu.js
+++ js/fetchMenu.js
@@ -1,5 +1,5 @@
 import {fetchHTMLContent, displayHTMLContent} from './../../../js/fetchHTMLContent.js';
 
-let resultText = await fetchHTMLContent('../../../js/menu.html');
+fetchHTMLContent('../../../js/menu.html');
 let insertInto = document.getElementById('menu');
 resultText.then(displayHTMLContent(resultText, insertInto));
