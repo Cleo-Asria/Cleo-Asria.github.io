e92e72857579f962f2d495e218d4bd8e760804a3
Judy <briggstaillefer10@gmail.com>
2025-12-30 10:14:33 -0800

    Update fetchHTMLContent.js

--- js/fetchHTMLContent.js
+++ js/fetchHTMLContent.js
@@ -1,11 +1,13 @@
-export async function fetchHTMLContent(file) {
+async function fetchHTMLContent(file) {
   const resultFile = await fetch(file);
-  export const resultText = await resultFile.text();
+  const resultText = await resultFile.text();
 }
 
-export function displayHTMLContent(resultText, insertTo) {
+function displayHTMLContent(resultText, insertTo) {
   insertedHTML = resultText;
   currentNode = document.head;
   currentHTML = currentNode.innerHTML;
   currentNode.innerHTML = currentHTML + insertedHTML;
 }
+
+export {fetchHTMLContent, displayHTMLContent, resultFile, resultText}
