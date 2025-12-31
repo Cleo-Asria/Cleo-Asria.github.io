22b12619989a9c0e3a81d6b9ba4e70d69385ec7a
Judy <briggstaillefer10@gmail.com>
2025-12-30 21:51:21 -0800

    Update fetchFont.js

--- js/fetchFont.js
+++ js/fetchFont.js
@@ -1,4 +1,2 @@
-import {fetchHTMLContent, displayHTMLContent, resultFile, resultText} from './../../../js/fetchHTMLContent.js';
-
 fetchHTMLContent('../../../js/getfont.html');
 displayHTMLContent(resultText, document.head);
