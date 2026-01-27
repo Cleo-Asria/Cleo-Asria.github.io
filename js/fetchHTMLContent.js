ca9443933779ed76966304ec59ed7175cc2c3849
Erial <briggstaillefer10@gmail.com>
2026-01-27 10:58:04 -0800

    Update fetchHTMLContent.js

--- js/fetchHTMLContent.js
+++ js/fetchHTMLContent.js
@@ -1,11 +1,11 @@
 async function fetchHTMLContent(file) {
-  resultFile = await fetch(file);
+  let resultFile = await fetch(file);
   return await resultFile.text();
 }
 
 function displayHTMLContent(result, insertTo) {
-  insertedHTML = result;
-  currentNode = insertTo;
+  let insertedHTML = result;
+  let currentNode = insertTo;
   currentHTML = currentNode.innerHTML;
   currentNode.innerHTML = currentHTML + insertedHTML;
 }
