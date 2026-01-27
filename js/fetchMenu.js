2767d8be6ae7db5051dc607b41c670c11296a98c
Erial <briggstaillefer10@gmail.com>
2026-01-26 17:24:46 -0800

    Update fetchMenu.js

--- js/fetchMenu.js
+++ js/fetchMenu.js
@@ -1,2 +1,2 @@
 fetchHTMLContent('../../../js/getfont.html')
-.then(result => displayHTMLContent(result, document.getElementById('menu')));
+.then(result2 => displayHTMLContent(result2, document.getElementById('menu')));
