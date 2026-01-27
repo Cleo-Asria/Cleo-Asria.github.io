7aa826b38734e0560228596c64cf985b8db7c17b
Erial <briggstaillefer10@gmail.com>
2026-01-26 16:56:53 -0800

    Update fetchFont.js

--- js/fetchFont.js
+++ js/fetchFont.js
@@ -1,6 +1,2 @@
-insertTo = [];
-item = 0;
-insertTo[item] = document.head;
-
 fetchHTMLContent('../../../js/getfont.html')
-.then(displayHTMLContent);
+.then(displayHTMLContent(result, document.head));
